445f89c60cea5d04c7c05481e71c4c52e12beba3
york998 <837774941@qq.com>
2017-09-27 19:52:29 -0500

    bigInitegerMutiply

--- ArrayString/src/array/T2sume.java
+++ ArrayString/src/array/T2sume.java
@@ -1,5 +1,11 @@
 package array;
 
-public class T2sume {
+import java.util.Arrays;
 
+public class T2sume {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean[] s = new boolean[9];
+		System.out.print(Arrays.toString(s));
+	}
 }
--- DynamicProgram/src/stringBreak/WordBreakI.java
+++ DynamicProgram/src/stringBreak/WordBreakI.java
@@ -11,7 +11,7 @@ public class WordBreakI {
 		//System.out.print(dict.toString());
 		res = new boolean[s.length()];
 		res[s.length()-1]=true;
-		for (int i = 0; i < s.length()-1; i++){
+		for (int i = 0; i <= s.length()-1; i++){
 	        res[i] = false;
 	    }
 		this.dpFunction(s, dict);
@@ -19,26 +19,32 @@ public class WordBreakI {
 	}
 	public boolean dpFunction(String s, Set<String> dict){
 		if(s.length()==0 || s == null) return true;
-		System.out.print(Arrays.toString(res)+" "+s.length());
+		//System.out.print(Arrays.toString(res)+" "+s.length()+"\n");
 		for(int i = s.length()-1; i>=0 ; i--){
-			if(res[i] && dict.contains(s.substring(i))){
+			if(dict.contains(s.substring(i))){
+				
+				//if(s.length()==3)System.out.println(res[i]);
 				if(i==0) {res[0]=true; return true;}
-				if(dpFunction(s.substring(0, i), dict)) {
-					res[i-1]=true;
-			// cut branch
+				if(res[i]){
+					break;
+				}else if(dpFunction(s.substring(0, i), dict)){
+					res[i]=true;
 					break;
 				}
+					
+					
 			}
 		}
 		return res[0];
 	}
 	
-
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String s = "leetcode";
-		String[] dict = {"leet", "code"};
-		
+		//String[] dict = {"leet", "code"};
+		s="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+		String[] dict={"a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"};
 		WordBreakI t = new WordBreakI();
 		boolean res = t.wordBreak(s, Arrays.asList(dict));
 		System.out.println(res);
